bdb22e7ff08bcaf15de8f55a63103e969cfb9f87
scvssa <romankem753@gmail.com>
2026-04-21 23:20:11 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.42%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        prev2 = nums[0]
+        prev1 = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            current = max(prev1, prev2 + nums[i])
+            prev2 = prev1
+            prev1 = current
+
+        return prev1       
